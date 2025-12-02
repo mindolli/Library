3eedb9563eacb43c330a2b7480a11dccbf1767b1
mindolli <alsehfdlrnldudnj@gmail.com>
2025-12-02 11:26:06 +0900

    12/2 수업 시간 중 한 것

--- src/sad/Book.java
+++ src/sad/Book.java
@@ -1,4 +1,40 @@
 package sad;
 
 public class Book {
+    // 필드
+    String title;
+    String author;
+    String publisher;
+    int seriesNumber;
+    String difficulty;
+    int genre;
+    boolean isBorrowable;
+    String bookType;
+
+    // 메서드
+    // 생성자
+    public Book(String title, String author, String publisher, int seriesNumber, String difficulty, int genre, boolean isBorrowable, String bookType) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.seriesNumber = seriesNumber;
+        this.difficulty = difficulty;
+        this.genre = genre;
+        this.isBorrowable = isBorrowable;
+        this.bookType = bookType;
+    }
+
+    // toString() - 도서 정보 출력용
+    @Override
+    public String toString() {
+        return "제목: " + title +
+                ", 저자: " + author +
+                ", 출판사: " + publisher +
+                ", 권수: " + seriesNumber +
+                ", 난이도: " + difficulty +
+                ", 장르: " + genre +
+                ", 대출 가능: "  + isBorrowable +
+                ", 책 종류: " + bookType;
+
+    }
 }
--- src/sad/Main.java
+++ src/sad/Main.java
@@ -1,4 +1,36 @@
 package sad;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Book> bookList = new ArrayList<>();
+
+
+        System.out.println("제목을 입력하세요.");
+        String title = scan.nextLine();
+        System.out.println("저자를 입력하세요.");
+        String author = scan.nextLine();
+        System.out.println("출판사를 입력하세요.");
+        String publisher = scan.nextLine();
+        System.out.println("시리즈의 권수를 입력하세요.");
+        int seriserNumber = scan.nextInt();
+        scan.nextLine();
+        System.out.println("난이도를 입력하세요.");
+        String difficulty = scan.nextLine();
+        System.out.println("장르를 입력하세요.");
+        int genre = scan.nextInt();
+        scan.nextLine();
+        System.out.println("대출가능 여부를 입력하세요.");
+        boolean isBorrowable = scan.nextBoolean();
+        scan.nextLine();
+        System.out.println("책 종류를 입력하세요.");
+        String bookType = scan.nextLine();
+        Book book = new Book(title , author,  publisher, seriserNumber, difficulty, genre, isBorrowable,bookType) ;
+        System.out.println(book);
+        bookList.add(book);
+
+    }
 }
